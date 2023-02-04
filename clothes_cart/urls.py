c813ff35753dccb751c495b6a6019c985581165d
ndrndrs <aey.ndrs@gmail.com>
2023-02-04 18:01:12 +0300

    update with some views

--- clothes_cart/urls.py
+++ clothes_cart/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import cart, add_to_cart, remove_from_cart
+from .views import cart, add_to_cart, remove_item_from_cart, delete_from_cart
 
 
 urlpatterns = [
     path('', cart, name='cart'),
     path('add_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
-    path('del_cart/<int:product_id>/', remove_from_cart, name='remove_from_cart'),
+    path('del_cart/<int:product_id>/', delete_from_cart, name='delete_from_cart'),
+    path('rem_item/<int:product_id>/', remove_item_from_cart, name='remove_item_from_cart'),
 ]
