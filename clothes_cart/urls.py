8d2c2af876b3e162e1d4ad45f0c9a1e8d78fb496
ndrndrs <aey.ndrs@gmail.com>
2023-02-04 17:34:12 +0300

    add 'remove' url

--- clothes_cart/urls.py
+++ clothes_cart/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import cart, add_to_cart
+from .views import cart, add_to_cart, remove_from_cart
 
 
 urlpatterns = [
     path('', cart, name='cart'),
     path('add_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
+    path('del_cart/<int:product_id>/', remove_from_cart, name='remove_from_cart'),
 ]
