3faea45d48c3f59140025985c6de20e5be1d24dc
ndrndrs <aey.ndrs@gmail.com>
2023-02-05 15:24:27 +0300

    create context_processors for navigation cart view

---
+++ clothes_cart/context_processors.py
@@ -0,0 +1,22 @@
+from .models import Cart, Item
+from .views import _cart_session_id
+
+
+def items_counter(request):
+    count = 0
+    if 'admin' in request.path:
+        return
+    else:
+        try:
+            cart = Cart.objects.filter(cart_id=_cart_session_id(request))
+            cart_items = Item.objects.all().filter(cart=cart[:1])
+            for item in cart_items:
+                count += item.quantity
+
+        except Cart.DoesNotExist:
+            count = 0
+
+    counter = {
+        'count': count
+    }
+    return counter
