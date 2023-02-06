c1e379015b6b9af5e4288f19e0598317ceeab103
ndrndrs <aey.ndrs@gmail.com>
2023-02-06 21:50:17 +0300

    add one more model in admin

--- products/admin.py
+++ products/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Parameter
 
 
 # Register your models here.
 
+
 class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('product_name',)}
     list_display = [
-        'category', 'product_name', 'price', 'quantity', 'is_available', 'modified_date', 'image', 'image_img'
+        'category', 'product_name', 'price', 'quantity',
+        'is_available', 'modified_date', 'image_img',
     ]
     list_display_links = [
         'product_name'
@@ -17,4 +19,15 @@ class ProductAdmin(admin.ModelAdmin):
     list_per_page = 35
 
 
+class ParameterAdmin(admin.ModelAdmin):
+    list_display = (
+        'product', 'category_param', 'value_param', 'created_date','is_available'
+    )
+    list_editable = ('is_available', )
+
+    list_filter = (
+        'product', 'category_param', 'value_param',
+    )
+
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Parameter, ParameterAdmin)
