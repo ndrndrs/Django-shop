f2a34d16a9ecd0e5bde2be7379dfc00c28c2e42f
ndrndrs <aey.ndrs@gmail.com>
2023-02-06 21:49:43 +0300

    cart admin

--- clothes_cart/admin.py
+++ clothes_cart/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from .models import Cart, Item
 # Register your models here.
-admin.site.register(Cart)
-admin.site.register(Item)
+
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = [
+        'cart_id', 'date_added'
+    ]
+
+class CartItem(admin.ModelAdmin):
+    list_display = [
+        'cart', 'product',  'quantity'
+    ]
+
+admin.site.register(Cart, CartAdmin)
+admin.site.register(Item, CartItem)
