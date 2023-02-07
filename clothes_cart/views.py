28d3dce11ebd55f1453515c0fa10d38381fa3faf
ndrndrs <aey.ndrs@gmail.com>
2023-02-07 21:00:44 +0300

    increase the same item quantity

--- clothes_cart/views.py
+++ clothes_cart/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponse
 
-
-from products.models import Product
+from products.models import Product, Parameter
 from .models import Cart, Item
 
 
@@ -15,30 +15,64 @@ def _cart_session_id(request):
 
 def add_to_cart(request, product_id):
     product = Product.objects.get(id=product_id)
+    product_param = []
+    if request.method == 'POST':
+        for item in request.POST:
+            key = item
+            value = request.POST[key]
+
+            try:
+                variation = Parameter.objects.get(product=product, category_param__iexact=key,
+                                                  value_param__iexact=value)
+                product_param.append(variation)
+            except:
+                ...
     try:
+        product = Product.objects.get(id=product_id)
         cart = Cart.objects.get(cart_id=_cart_session_id(request))
     except Cart.DoesNotExist:
         cart = Cart.objects.create(
             cart_id=_cart_session_id(request)
         )
         cart.save()
-    try:
-        cart_item = Item.objects.get(product=product, cart=cart)
-        cart_item.quantity += 1
-        cart_item.save()
-    except Item.DoesNotExist:
+
+    check_item = Item.objects.filter(product=product, cart=cart).exists()
+    if check_item:
+        cart_item = Item.objects.filter(product=product, cart=cart)
+        par_list = []
+        id = []
+        for item in cart_item:
+            existing_variation = item.parameters.all()
+            par_list.append(list(existing_variation))
+            id.append(item.id)
+
+        if product_param in par_list:
+            idx = par_list.index(product_param)
+            item_id = id[idx]
+            item = Item.objects.get(product=product, id=item_id)
+            item.quantity += 1
+            item.save()
+        else:
+            if len(product_param) > 0:
+                cart_item.parameters.clear()
+                cart_item.parameters.add(*product_param)
+            cart_item.save()
+    else:
         cart_item = Item.objects.create(
             product=product,
             cart=cart,
             quantity=1,
         )
+        if len(product_param) > 0:
+            cart_item.parameters.clear()
+            cart_item.parameters.add(*product_param)
         cart_item.save()
     return redirect('cart')
+
     # return HttpResponse(cart_item.product) check add
 
 
 def remove_item_from_cart(request, product_id):
-
     cart = Cart.objects.get(cart_id=_cart_session_id(request))
     product = Product.objects.get(id=product_id)
     cart_item = Item.objects.get(product=product, cart=cart)
@@ -49,12 +83,14 @@ def remove_item_from_cart(request, product_id):
         cart_item.delete()
     return redirect('cart')
 
+
 def delete_from_cart(request, product_id):
     cart = Cart.objects.get(cart_id=_cart_session_id(request))
     product = get_object_or_404(Product, id=product_id)
     cart_item = Item.objects.get(product=product, cart=cart)
     cart_item.delete()
     return redirect('cart')
+
 
 def cart(request, total=0, quantity=0, cart_items=None, tax=0):
     try:
@@ -77,4 +113,3 @@ def cart(request, total=0, quantity=0, cart_items=None, tax=0):
         'cart_items': cart_items,
     }
     return render(request, 'store/cart.html', context)
-
