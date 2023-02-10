5765d9d0e558c7f4de7724937c96b595ac9f721f
ndrndrs <aey.ndrs@gmail.com>
2023-02-10 17:03:24 +0300

    correct some urls

--- clothes_cart/urls.py
+++ clothes_cart/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', cart, name='cart'),
     path('add_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
     path('del_cart/<int:product_id>/', delete_from_cart, name='delete_from_cart'),
-    path('rem_item/<int:product_id>/', remove_item_from_cart, name='remove_item_from_cart'),
+    path('rem_item/<int:product_id>/z', remove_item_from_cart, name='remove_item_from_cart'),
 ]
--- custom_user/urls.py
+++ custom_user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import register, login, logout
+from .views import register, login, logout, activate
 
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login, name='login'),
     path('logout/', logout, name='logout'),
+    path('activate/<uidb64>/<token>/', activate, name='activate'),
 ]
